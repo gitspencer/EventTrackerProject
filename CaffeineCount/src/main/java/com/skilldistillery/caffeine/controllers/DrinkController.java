c99692b22ce4ed68f4040c8bdef764408abcdf30
Spencer Williams <srwilliams349@gmail.com>
2023-06-04 23:09:59 -0400

    add method working. something wrong with error catching, commented out and will troubleshoot after completing full CRUD

--- CaffeineCount/src/main/java/com/skilldistillery/caffeine/controllers/DrinkController.java
+++ CaffeineCount/src/main/java/com/skilldistillery/caffeine/controllers/DrinkController.java
@@ -2,6 +2,7 @@ package com.skilldistillery.caffeine.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,24 @@ public class DrinkController {
 	}
 	
 	@PostMapping("drinks")
+	public Drink addDrink(@RequestBody Drink drink, HttpServletResponse res, HttpServletRequest req) {
+//		try {
+			drink = drinkService.create(drink);
+//			if (drink == null) {
+//				res.setStatus(404);
+//			} else {
+//				res.setStatus(201);
+//				StringBuffer url = req.getRequestURL();
+//				url.append("/").append(drink.getId());
+//				res.setHeader("Location", url.toString());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			res.setStatus(400);
+//			drink = null;
+//		}
+		return drink;
+	}
 	
 	
 	@PutMapping("drinks/{id}")
